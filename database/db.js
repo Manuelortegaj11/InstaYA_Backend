ba6db7b53357eef951765ad4a6f85b4781a26319
xhlar <contacto@xhlar.com>
2022-12-06 22:30:53 -0500

    update db
    Signed-off-by: xhlar <contacto@xhlar.com>

--- database/db.js
+++ database/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-
-const MONGO_URL = "mongodb+srv:" + "//" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@cluster0.rfosq5q.mongodb.net/?retryWrites=true&w=majority";
 //mongodb+srv://proyectouninorte:<password>@cluster0.rfosq5q.mongodb.net/?retryWrites=true&w=majority
 const db = async() => {
+const MONGO_URL = "mongodb+srv:" + "//" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@cluster0.rfosq5q.mongodb.net/?retryWrites=true&w=majority";
   await mongoose
   .connect(MONGO_URL)
   .then(() => console.log("DB FUNCIONANDO"))
